e9e450a42945f1029ef44e15c87872461ba3e0c3
Seongil-Kim <seongil8@outlook.com>
2022-10-10 02:06:16 +0900

    Seongil Kim: Update

--- client/src/components/PropertyItemDetail.js
+++ client/src/components/PropertyItemDetail.js
@@ -3,65 +3,96 @@ import styled from "styled-components";
 
 import {useNavigate} from 'react-router-dom';
 
+import { useState, useEffect } from "react";
+
 const PropertyItemDetail=({propertyDetailInfoObj})=>{
 
     const navigator=useNavigate();
+    const [additionalInfo, setAdditionalInfo]=useState(null);
 
-    // The data from basic property detail which is included in prop will be showed without delay.
-    // But additional data from fetching will be showed later.
-    fetch(`/api/realty-info-detail/${propertyDetailInfoObj.property_id}`)
-    .then((res)=>res.json())
-    .then((data)=>{            
-        console.log("property detail: ", data.propertyDetail);            
-    })        
-    .catch((error)=>{
-        console.error("Error: ", error);
-    // errorHandler("error-receive");
+    // If those are in useEffect with a vacant array, the detail page is not updated except for the first click for an element, 
+    // because every code in useEffect runs only once.
+    useEffect(()=>{
+
+        // const promises=[];
+        
+        // The data from basic property detail which is included in prop will be showed without delay.
+        // But additional data from fetching will be showed later.
+        const propertyDetail=fetch(`/api/realty-info-detail/${propertyDetailInfoObj.property_id}`)
+        .then((res)=>res.json())
+        .then((data)=>{            
+            console.log("property detail: ", data.propertyDetail);
+            return data.propertyDetail;
+        })        
+        .catch((error)=>{
+            console.error("Error: ", error);
+        // errorHandler("error-receive");
+        });
+
+        // Request loan opportunity and interest info.
+        const rateInfo=fetch(`/api/mortgage/check-rate?price=${propertyDetailInfoObj.price}&zip=${propertyDetailInfoObj.address.postal_code}`)
+        .then((res)=>res.json())
+        .then((data)=>{            
+            console.log("Rate Info: ", data.rateInfo);  
+            return data.rateInfo;        
+        })        
+        .catch((error)=>{
+            console.error("Error: ", error);
+        // errorHandler("error-receive");
+        });
+
+        // console.log("var_propertyDetail: ", propertyDetail);
+        // console.log("var_rateInfo: ", rateInfo);       
+        
+        Promise.all([propertyDetail,rateInfo])
+        .then((result)=>{
+        console.log("result: ", result);
+        setAdditionalInfo({propertyDetail: result[0], rateInfo: result[1]});
     });
 
-    // Request loan opportunity and interest info.
-    fetch(`/api/mortgage/check-rate?price=${propertyDetailInfoObj.price}&zip=${propertyDetailInfoObj.address.postal_code}`)
-    .then((res)=>res.json())
-    .then((data)=>{            
-        console.log("Rate Info: ", data.rateInfo);            
-    })        
-    .catch((error)=>{
-        console.error("Error: ", error);
-    // errorHandler("error-receive");
-    });
+    },[propertyDetailInfoObj]);
+
+    
 
     return (
         <PropertyItemDetailShowDiv>
-            <PropertyItemDetailShow>
-                
-                {/* Info From Property List Element */}
-                Property Id: {propertyDetailInfoObj.property_id} <br/>
-                Property Type: {propertyDetailInfoObj.prop_type} <br/>
-                Property Status: {propertyDetailInfoObj.prop_status} <br/>
-                Price of Property: {propertyDetailInfoObj.price} <br/>
-                
-                Address: {propertyDetailInfoObj.address.line} {propertyDetailInfoObj.address.city}
-                         {propertyDetailInfoObj.address.state} <br/> 
-                Postal Code: {propertyDetailInfoObj.address.postal_code} <br/> 
-                
-                # of Beds: {propertyDetailInfoObj.beds} <br/> 
-                # of Bath: {propertyDetailInfoObj.baths} <br/> 
 
-                {/* Lot Size:  {propertyDetailInfoObj.lot_size.size} {propertyDetailInfoObj.lot_size.units} <br/>  */}
+            {additionalInfo && 
+                <PropertyItemDetailShow>
+                    
+                    {/* Info From Property List Element */}
+                    Property Id: {propertyDetailInfoObj.property_id} <br/>
+                    Property Type: {propertyDetailInfoObj.prop_type} <br/>
+                    Property Status: {propertyDetailInfoObj.prop_status} <br/>
+                    Price of Property: {propertyDetailInfoObj.price} <br/>
+                    
+                    Address: {propertyDetailInfoObj.address.line} {propertyDetailInfoObj.address.city}
+                            {propertyDetailInfoObj.address.state} <br/> 
+                    Postal Code: {propertyDetailInfoObj.address.postal_code} <br/> 
+                    
+                    # of Beds: {propertyDetailInfoObj.beds} <br/> 
+                    # of Bath: {propertyDetailInfoObj.baths} <br/> 
 
-                Agent Id: {propertyDetailInfoObj.agents[0].advertiser_id} <br/> 
-                Agent: {propertyDetailInfoObj.agents[0].name} <br/> 
-                {/* Agent Photo: <img src={propertyDetailInfoObj.agents[0].photo.href}/> <br/> */}
+                    {/* Lot Size:  {propertyDetailInfoObj.lot_size.size} {propertyDetailInfoObj.lot_size.units} <br/>  */}
 
-                RDC Web: {propertyDetailInfoObj.rdc_web} <br/> 
+                    Agent Id: {propertyDetailInfoObj.agents[0].advertiser_id} <br/> 
+                    Agent: {propertyDetailInfoObj.agents[0].name} <br/> 
+                    {/* Agent Photo: <img src={propertyDetailInfoObj.agents[0].photo.href}/> <br/> */}
 
-                {/* Info From Property Detail fetched by property_id */}
+                    RDC Web: {propertyDetailInfoObj.rdc_web} <br/> 
 
+                    {/* Info From Property Detail fetched by property_id */}
+                    Agent Office: <img src={additionalInfo.propertyDetail.properties[0].branding.listing_office.details.photo}/> 
+                                  {additionalInfo.propertyDetail.properties[0].branding.listing_office.details.name} <br/> 
+                    Agent Phone: {additionalInfo.propertyDetail.properties[0].branding.listing_office.details.phone} <br/> 
+                    Agent Link: {additionalInfo.propertyDetail.properties[0].branding.listing_office.details.link}
+                    
 
-                {/* Info from Rate Info fetched by zip code and property price(assumed 80% loan percent) */}
+                    {/* Info from Rate Info fetched by zip code and property price(assumed 80% loan percent) */}
 
-                
-            </PropertyItemDetailShow>
+                    
+                </PropertyItemDetailShow>
+            }
             <MortgageCalButton onClick={(event)=>{
                                     event.preventDefault(); //to remove default function from parent.
                                     navigator("/calcul-mortgage");
